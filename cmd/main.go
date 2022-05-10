2b8152aa6e52680c9b7eb0d6cb99a42cf68c95af
greetdawn <chengqunpeter@gmail.com>
2022-05-10 11:16:31 +0800

    host alive

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"poodle/pkg/parser"
-)
-
 func main() {
+	IsHostAlived("123")
 	// 单IP嗅探
-	parser.Parseing(10010000, []string{"192.168.1.1"})
+	// 	parser.Parseing(10010000, []string{"192.168.1.1"})
 
-	// 单域名嗅探测试
-	parser.Parseing(10020000, []string{"baidu.com"})
+	// 	// 单域名嗅探测试
+	// 	parser.Parseing(10020000, []string{"baidu.com"})
 }
