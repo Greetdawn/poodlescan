80531c0ac0dba6c20fd7212c0c5c69d724d01f61
koifishly <koifishly@outlook.com>
2022-05-10 13:13:02 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,11 @@
 package main
 
-import "poodle/pkg/common"
+import "poodle/pkg/parser"
 
 func main() {
-	common.IsHostAlived("")
 	// 单IP嗅探
-	// 	parser.Parseing(10010000, []string{"192.168.1.1"})
+	parser.Parseing(10010000, []string{"192.168.1.1"})
 
 	// 	// 单域名嗅探测试
-	// 	parser.Parseing(10020000, []string{"baidu.com"})
+	parser.Parseing(10020000, []string{"baidu.com"})
 }
