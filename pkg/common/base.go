232246a3c592c560fdc174398595671fa0076f31
koifishly <koifishly@outlook.com>
2022-05-16 15:22:52 +0800

    控制码定义完成

--- pkg/common/base.go
+++ pkg/common/base.go
@@ -25,7 +25,37 @@ var DOMAINARRAY []string = []string{
 
 // uint32每个bit代表的功能
 // CC -- Control Code
-// 0: Ping扫功能
-// 1: 端口扫描
-const CC_PING_SCAN uint = 1
-const CC_Port_Scan uint = 2
+const (
+	// 0: Ping扫功能
+	CC_PING_SCAN uint = 1
+	// 1: 端口扫描
+	CC_PORT_SCAN uint = 2
+	// 2: 子域探测功能
+	CC_SUB_DOMAIN_SCAN uint = 4
+	// 3: 爬虫功能
+	CC_SPIDER uint = 8
+	// 4: 指纹识别功能
+	CC_FINGERPRINT uint = 16
+	// 5: 保留
+	// 6: 保留
+	// 7: 保留
+
+	// 漏洞扫描模块
+	// 8: 专项漏洞扫描功能, 针对具体漏洞编号扫描
+	CC_VULSCAN_ID uint = 256
+	// 9: 专项漏洞扫描功能, 针对具体的漏洞类型扫描（例如：weblogic）
+	CC_VULSCAN_TYPE uint = 512
+	//10: 专项漏洞扫描功能, 根据用户自定义漏洞文件路径扫描
+	CC_VULSCAN_FILE uint = 1024
+	//11: 专项漏洞扫描功能, 针对web应用开启目录探测功能
+	CC_VULSCAN_DIRSEARCH uint = 2048
+	//12: 服务端口爆破功能, 针对应用服务端口进行口令爆破
+	CC_VULSCAN_BURST uint = 4096
+	//13: 保留
+	//14: 保留
+	//15: 保留
+
+	// 漏洞利用模块
+	//16:漏洞利用功能
+	CC_VULEXPLOIT uint = 65536
+)
