95695e5ad9b09165e321ac6492008f35943c7c36
greetdawn <953640552@qq.com>
2022-05-18 14:07:03 +0800

    更新完成整个解析器

--- pkg/cmd_parser/base.go
+++ pkg/cmd_parser/base.go
@@ -1,6 +1,9 @@
 package cmdparser
 
-import "poodle/pkg/common"
+import (
+	"fmt"
+	"poodle/pkg/common"
+)
 
 // 全局变量，保存终端命令行参数结构体
 var G_TerminalParam TerminalParams
@@ -8,6 +11,8 @@ var G_TerminalParam TerminalParams
 // 命令行参数结构体
 // 在用户输入后，通过flag模块，将用户输入的命令行转换成结构体保存
 type TerminalParams struct {
+	// 指定全流程扫描
+	Full bool
 	// 用户输入的扫描目标，原始字符串
 	UserInputTargetString string
 	// 标志;一些特殊的标志
@@ -31,12 +36,46 @@ type TerminalParams struct {
 	// 指定是否进行指纹识别
 	Fingerprint bool
 	// 指定全漏洞扫描
-	vulscan bool
+	Vulscan bool
+	// 指定具体漏洞名称或者编号扫描
+	Vulscanid string
+	// 指定具体漏洞类型扫描
+	VulscanType string
+	// 指定用户自定义脚本扫描
+	VulscanDefined string
+	// 指定目录探测功能
+	VulscanDirsearch bool
+	// 指定服务端口爆破
+	VulscanBurst bool
+	// 指定具体漏洞利用脚本
+	Vulexploit string
+	// 命令执行需要的参数
+	Command string
 }
 
 // 根据终端参数结构体生成控制码
 func (this *TerminalParams) GenerateControlCode() (controlCode uint) {
 	controlCode = 0
+
+	// -F 定义全流程扫描
+	if this.Full {
+		this.Sniffer = true
+		this.Vulscan = true
+	}
+
+	// -sn 定义嗅探扫描全流程
+	if this.Sniffer {
+		this.PortScan = true
+		this.SubDomain = true
+		this.Spider = true
+		this.Fingerprint = true
+	}
+
+	// -vs 定义全漏洞扫描流程
+	if this.Vulscan {
+		this.VulscanDirsearch = true
+		this.VulscanBurst = true
+	}
 
 	// -Pn 	跳过主机存活检测
 	// 默认不跳过
@@ -44,20 +83,44 @@ func (this *TerminalParams) GenerateControlCode() (controlCode uint) {
 		controlCode |= common.CC_PING_SCAN
 	}
 
-	// 端口扫描
-	controlCode |= common.CC_PORT_SCAN
+	// 指定端口扫描功能
+	if this.PortScan {
+		controlCode |= common.CC_PORT_SCAN
+	}
 
 	// 子域探测
-	controlCode |= common.CC_SUB_DOMAIN_SCAN
+	if this.SubDomain {
+		controlCode |= common.CC_SUB_DOMAIN_SCAN
+	}
 
 	// 爬虫功能
-	controlCode |= common.CC_SPIDER
+	if this.Spider {
+		controlCode |= common.CC_SPIDER
+	}
 
 	// 指纹识别
-	controlCode |= common.CC_FINGERPRINT
+	if this.Fingerprint {
+		controlCode |= common.CC_FINGERPRINT
+	}
 
 	// 全漏洞扫描
-	controlCode |= common.CC_VULSCAN
+	if this.Vulscan {
+		controlCode |= common.CC_VULSCAN
+		controlCode = controlCode ^ common.CC_PING_SCAN
+	}
 
+	// 指定目录探测扫描
+	if this.VulscanDirsearch {
+		controlCode |= common.CC_VULSCAN_DIRSEARCH
+		controlCode = controlCode ^ common.CC_PING_SCAN
+	}
+
+	// 指定服务端口爆破
+	if this.VulscanBurst {
+		controlCode |= common.CC_VULSCAN_BURST
+		controlCode = controlCode ^ common.CC_PING_SCAN
+	}
+
+	fmt.Println(controlCode)
 	return controlCode
 }
--- pkg/cmd_parser/cmd_parse.go
+++ pkg/cmd_parser/cmd_parse.go
@@ -49,41 +49,70 @@ func ParsingUserTerminalLine() (terminalParams TerminalParams, err error) {
 	// 获取终端参数的数量
 	numberArgs := len(os.Args)
 
+	// -f : 定义全流程扫描
+	flag.BoolVar(&terminalParams.Full, "F", false, "指定全流程探测扫描")
+
 	// -t : 设置用户提供的目标
-	flag.StringVar(&terminalParams.UserInputTargetString, "t", "", "设置扫描目标")
+	flag.StringVar(&terminalParams.UserInputTargetString, "t", "127.0.0.1", "扫描目标 例如: ip:192.168.1.0/24,192.168.1.10-50或直接输入域名 greetdawn.top (default 127.0.0.1)")
 
 	// -Pn : 如果加了这个参数，则表示跳过Ping扫
 	flag.BoolVar(&terminalParams.IsPn, "Pn", false, "跳过ping扫")
 
 	// -T : 用户设置线程
-	flag.IntVar(&terminalParams.ThreadsNumber, "T", 5, "设置并发，允许同时扫描几个目标")
+	flag.IntVar(&terminalParams.ThreadsNumber, "T", 100, "设置并发，允许同时扫描几个目标(100)")
 
 	// -sn : 指定完整嗅探扫描
-	flag.BoolVar(&terminalParams.Sniffer, "sn", false, "指定完整嗅探扫描")
+	flag.BoolVar(&terminalParams.Sniffer, "sn", false, "指定完整嗅探扫描功能")
 
 	// -sn-p ：指定端口扫描
-	flag.BoolVar(&terminalParams.PortScan, "sn-p", false, "指定端口扫描")
+	flag.BoolVar(&terminalParams.PortScan, "sn-p", false, "指定端口扫描功能")
 
 	// -p : 指定端口扫描端口范围
 	flag.StringVar(&terminalParams.PortList, "p", "top1000", "指定扫描端口范围 例如:80,8080,80-1000 (default top1000)")
 
 	// -sn-sub : 指定子域探测
-	flag.BoolVar(&terminalParams.SubDomain, "sn-sub", false, "指定子域探测")
+	flag.BoolVar(&terminalParams.SubDomain, "sn-sub", false, "指定子域探测功能")
 
 	// -sn-sp : 指定网站爬虫
-	flag.BoolVar(&terminalParams.Spider, "sn-sp", false, "指定网站爬虫")
+	flag.BoolVar(&terminalParams.Spider, "sn-sp", false, "指定网站爬虫功能")
 
 	// -sn-fp : 指定指纹识别
-	flag.BoolVar(&terminalParams.Fingerprint, "sn-fp", false, "指定指纹识别")
+	flag.BoolVar(&terminalParams.Fingerprint, "sn-fp", false, "指定指纹识别功能")
 
 	// -vs : 指定全漏洞扫描
-	flag.BoolVar(&terminalParams.vulscan, "vs", false, "指定全漏洞扫描")
+	flag.BoolVar(&terminalParams.Vulscan, "vs", false, "指定全漏洞扫描功能")
 
-	// -vs-id : 指定具体漏洞编号扫描 例如：cve-2021-44228 (default "空值")
+	// -vs-id : 指定具体漏洞编号或名称扫描 例如：cve-2021-44228 (default "空值")
+	flag.StringVar(&terminalParams.Vulscanid, "vs-id", "", "指定具体漏洞编号或名称扫描 例如: cve-2021-44228 (default 空值)")
+
+	// -vs-t : 指定具体漏洞类型扫描 例如：webligic (default "空值")
+	flag.StringVar(&terminalParams.VulscanType, "vs-t", "", "指定具体漏洞类型扫描 例: weblogic (default 空值)")
+
+	// -vs-def : 指定用户自定义脚本扫描 例如：path/pocname (default "空值")
+	flag.StringVar(&terminalParams.VulscanDefined, "vs-def", "", "指定具体漏洞类型扫描 例: weblogic (default 空值)")
+
+	// -vs-dir : 指定目录探测功能
+	flag.BoolVar(&terminalParams.VulscanDirsearch, "vs-dir", false, "指定目录探测功能")
+
+	// -vs-b : 指定服务端口爆破
+	flag.BoolVar(&terminalParams.VulscanBurst, "vs-b", false, "指定服务端口爆破功能")
+
+	// -ve : 指定具体漏洞利用脚本
+	flag.StringVar(&terminalParams.Vulexploit, "ve", "", "指定具体漏洞利用脚本 例如: cve-2021-44228-exp (default 空值)")
+
+	// -c : 命令执行需要参数
+	flag.StringVar(&terminalParams.Vulexploit, "c", "whoami", "执行命令 例如：whoami （default whoami）")
 
 	// 开始解析
 	flag.Parse()
 
+	// 根据用户参数输入的数量判断执行完整扫描流程
+	if numberArgs == 1 {
+		logger.LogWarn("参数输入不正确, 请输入 -h 获取工具帮助信息", logger.LOG_TERMINAL)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%#v\n", os.Args)
 	fmt.Printf("%d\n", numberArgs)
 	fmt.Printf("terminalParams: %v\n", terminalParams)
 
