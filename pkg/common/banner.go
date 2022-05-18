eb5d83cc8c660e5a66fd2dfa7e84193f4055ecde
koifishly <koifishly@outlook.com>
2022-05-18 15:08:41 +0800

    feat: cls

--- pkg/common/banner.go
+++ pkg/common/banner.go
@@ -2,6 +2,9 @@ package common
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 var banner = `
@@ -14,6 +17,20 @@ var banner = `
 `
 
 func ShowBanner() {
+	optSys := runtime.GOOS
+	if optSys == "linux" {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 	fmt.Println(banner)
 	fmt.Printf("\t\t\t\t\t\t\t\t\tBy Teams\n\n")
 }
